711a85e3f43a9d57c1228744d4779ea4497d1cdd
Gregory Peterson <pet03356@morris.umn.edu>
2022-12-05 12:04:20 -0600

    Disemvowel.c is executable but it's not working for stdin and stdout

--- file_disemvowel/disemvowel.c
+++ file_disemvowel/disemvowel.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -14,29 +17,31 @@ bool is_vowel(char c) {
 }
 
 int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
-	int count=0
+	int count=0;
 	for(int i=0; i<num_chars; i++){
 		if(is_vowel(in_buf[i])){
 		out_buf[count] = in_buf[i];
-		count++
+		count++;
 		}
 	}
 	return count;
 }
 
 void disemvowel(FILE* inputFile, FILE* outputFile) {
-    /*
-     * Copy all the non-vowels from inputFile to outputFile.
-     * Create input and output buffers, and use fread() to repeatedly read
-     * in a buffer of data, copy the non-vowels to the output buffer, and
-     * use fwrite to write that out.
-     */
+
+	
+	char in_buf[BUF_SIZE];
+	char out_buf[BUF_SIZE];
+	int charCount = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+
+	while(charCount != 0){
+		int count = copy_non_vowels(charCount, in_buf, out_buf);
+		fwrite(out_buf, sizeof(char), count, outputFile);
+		charCount = fread(in_buf, sizeof(char), BUF_SIZE, inputFile);
+	}
 }
 
 int main(int argc, char *argv[]) {
-    // This sets these to `stdin` and `stdout` by default.
-    // You then need to set them to user specified files when the user
-    // provides files names as command line arguments.
     FILE *inputFile;
     FILE *outputFile;
 
@@ -47,11 +52,18 @@ int main(int argc, char *argv[]) {
 
     if(argc == 2){
 	    inputFile = fopen(argv[1], "r");
+	    outputFile = stdout;
+    }
 
-    // Code that processes the command line arguments
-    // and sets up inputFile and outputFile.
+    if (argc == 3){
+	    inputFile = fopen(argv[1], "r");
+	    outputFile = fopen(argv[2], "w");
+    }
 
     disemvowel(inputFile, outputFile);
+
+    fclose(inputFile);
+    fclose(outputFile);
 
     return 0;
 }
